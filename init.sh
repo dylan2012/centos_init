acd5396435ab88044a7d69b2669394e88d1def35
dylan2012 <dylan865078@gmail.com>
2021-07-30 16:10:03 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -138,6 +138,7 @@ setSELinux() {
 
 dockerINSTALL() {
 	curl -fsSL https://get.docker.com/ | sh
+	systemctl enable docker && systemctl start docker
 	sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && chmod +x  /usr/local/bin/docker-compose
 }
 
